b463ad171b7ff7b0752a20885c8509d0e50df29d
Alexsandr <artamoshkinas@gmail.com>
2023-04-06 15:48:58 +0700

    Написан код для запуска приложения

--- notepad.py
+++ notepad.py
@@ -1,1 +1,20 @@
+import sys
 
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from GUI.GUI import Ui_mainWindow
+
+class Notepad(QApplication):
+    def __init__(self):
+        super(Notepad, self).__init__()
+        self.ui = Ui_mainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    ui = Ui_mainWindow()
+    ui.setupUi(window)
+
+    window.show()
+    sys.exit(app.exec_())
