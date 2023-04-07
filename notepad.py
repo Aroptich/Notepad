34976a9d3b7cd2def6c9f0cf750def1c8331caea
Alexsandr <artamoshkinas@gmail.com>
2023-04-07 19:39:25 +0700

    Добавлено: Добавлена возможность заполнения таблицы и очистка полей после ввода

--- notepad.py
+++ notepad.py
@@ -1,20 +1,36 @@
 import sys
+from datetime import datetime
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 from GUI.GUI import Ui_mainWindow
 
-class Notepad(QApplication):
+
+class Notepad(QMainWindow):
     def __init__(self):
         super(Notepad, self).__init__()
         self.ui = Ui_mainWindow()
         self.ui.setupUi(self)
 
+        self.ui.btn_create.clicked.connect(self.create_note)
+
+    def create_note(self):
+        rowPosition = self.ui.table_notes.rowCount()
+        self.ui.table_notes.insertRow(rowPosition)
+
+        text_title = self.ui.lineEdit.text()
+        text_note = self.ui.field_note.toPlainText()
+
+        write_title = self.ui.table_notes.setItem(rowPosition, 0, QTableWidgetItem(text_title))
+        write_date_time = self.ui.table_notes.setItem(rowPosition, 1,
+                                                      QTableWidgetItem(str(datetime.now().strftime("%d-%m-%Y %H:%M"))))
+
+        self.ui.field_note.clear()
+        self.ui.lineEdit.clear()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = QMainWindow()
-    ui = Ui_mainWindow()
-    ui.setupUi(window)
-
+    window = Notepad()
     window.show()
     sys.exit(app.exec_())
