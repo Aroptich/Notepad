8544d5ebd847ecc12fa0bff0ba7c18809c1daae6
Alexsandr <artamoshkinas@gmail.com>
2023-04-07 21:04:20 +0700

    Добавлено: Добавлен функционал создания json-файла для хранения данных

--- notepad.py
+++ notepad.py
@@ -1,4 +1,6 @@
+import os.path
 import sys
+import json
 from datetime import datetime
 
 from PyQt5.QtCore import *
@@ -20,13 +22,24 @@ class Notepad(QMainWindow):
 
         text_title = self.ui.lineEdit.text()
         text_note = self.ui.field_note.toPlainText()
+        id = rowPosition + 1
 
-        write_title = self.ui.table_notes.setItem(rowPosition, 0, QTableWidgetItem(text_title))
-        write_date_time = self.ui.table_notes.setItem(rowPosition, 1,
+        write_title = self.ui.table_notes.setItem(rowPosition, 1, QTableWidgetItem(text_title))
+        write_date_time = self.ui.table_notes.setItem(rowPosition, 2,
                                                       QTableWidgetItem(str(datetime.now().strftime("%d-%m-%Y %H:%M"))))
+        write_id = self.ui.table_notes.setItem(rowPosition, 0, QTableWidgetItem(str(rowPosition + 1)))
+
+        self.create_json()
 
         self.ui.field_note.clear()
         self.ui.lineEdit.clear()
+
+    def create_json(self):
+        if not os.path.isfile("Json/db.txt"):
+            db = {}
+            with open("Json/db.txt", 'a', encoding="cp1251") as file:
+                json.dump(db, file)
+
 
 
 if __name__ == '__main__':
